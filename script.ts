3426fc1f89f4647d718389b863c6a457304ab739
Anas El Hajjaji <anas.el-hajjaji@exfo.com>
2019-10-03 15:03:24 -0400

    add promises

--- script.ts
+++ script.ts
@@ -14,3 +14,30 @@ console.log("//                          //");
 console.log("//--------------------------//");
 import { CarType } from "./interfaces";
 CarType.display();
+
+console.log("//--------------------------//");
+console.log("//                          //");
+console.log("//         PROMISES         //");
+console.log("//                          //");
+console.log("//--------------------------//");
+const promise1 = new Promise((resolve, reject) => {
+    resolve(123);
+});
+promise1.then((res) => {
+    console.log('I get called:', res === 123); // I get called: true
+    return 456;
+}).then((res) => {
+    console.log('I get called:', res === 456); // I get called: true
+});
+promise1.catch((err) => {
+    // This is never called
+});
+const promise2 = new Promise((resolve, reject) => {
+    reject(new Error("Something awful happened"));
+});
+promise2.then((res) => {
+    // This is never called
+});
+promise2.catch((err) => {
+    console.log('I get called:', err.message); // I get called: 'Something awful happened'
+});
